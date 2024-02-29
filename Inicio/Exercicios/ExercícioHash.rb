1592ba7a1edb9d555f23c70a1026eff9739faa6e
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-02-28 22:50:31 -0300

    atualizacoes

--- Inicio/Exercicios/ExercícioHash.rb
+++ Inicio/Exercicios/ExercícioHash.rb
@@ -1,5 +1,11 @@
-aluno = {nome: 'João', nota: 7, disciplina: 'Ciências'}
+aluno = {nome: 'João', nota: 3, disciplina: 'Ciências'}
 
 alunos =  [aluno,]
 
 puts "#{alunos[0][:nome]} tirou nota #{alunos[0][:nota]}"
+
+if aluno[:nota] >= 5
+    puts "#{:nome} foi aprovado em #{:disciplina}"
+else
+    puts "#{:nome} foi reprovado em #{:disciplina}" 
+end
